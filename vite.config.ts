c581d03040e5dbfb814758f485767e7af250bbd6
hiddenyak <hiddenyak@gmail.com>
2025-07-04 18:47:07 -0400

    feat: add deploy script and CNAME for GitHub Pages deployment
    - Added a deploy script to package.json to build the project and deploy to GitHub Pages.
    - Created a CNAME file in the public directory for custom domain configuration.
    - Removed unnecessary build configuration from vite.config.ts for cleaner setup.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
 
 export default defineConfig({
 	base: "/",
 	plugins: [vue()],
-	build: {
-		outDir: ".", // Output directly to root
-		emptyOutDir: false, // Don't delete everything in root when building
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "index.html"),
-			},
-		},
-	},
 });
