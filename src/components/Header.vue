49d1fe936d64a405296107a8c7849ae3185686f2
hiddenyak <hiddenyak@gmail.com>
2025-07-14 16:02:31 -0400

    responsive config

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,11 +16,15 @@
 	margin-bottom: 10px;
 	position: sticky;
 	z-index: 1000;
-	box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1); /* optional */
+	box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1);
+	overflow-x: auto; /* allows scrolling */
+	white-space: nowrap; /* keeps links in one line */
+	-webkit-overflow-scrolling: touch; /* smooth scrolling on iOS */
 }
 a {
 	color: white;
 	text-decoration: none;
+	padding: 0.5rem 0;
 }
 .router-link-active {
 	font-weight: bold;
